a8a55ba1dcb69147654d1c9d10e45eb05772f1be
maddielicht <mpl219@lehigh.edu>
2015-08-28 15:54:33 +0000

    updated HellowWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////
+//Madison Licht
+//Demo 0
+//Hello World Java Program
+//
+//   first compile the program
+//      javac HellowWorld.java
+//   run the program
+//      java HelloWorld//
+
+//   define a class
+public class HelloWorld{
+    
+//   add main methof
+  public static void main(String[] args) {
+      
+//   print te statement with box around it
+     System.out.println("#####################");
+     System.out.println("#####################");
+     System.out.println("#                   #");
+     System.out.println("#   Hello World!!   #");
+     System.out.println("#                   #");
+     System.out.println("#####################");
+     System.out.println("#####################");
+     System.out.println("test");
+
+     //test/testtest
+  }
+}

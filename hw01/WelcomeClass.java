25675115a94b27791dc880ec7b3d3b8ee21f2114
maddielicht <mpl219@lehigh.edu>
2015-08-30 20:17:56 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////
+//Madison Licht
+//Homework 1
+//Welcome screen with autobiography statement
+//
+//   first compile the program
+//      javac WelcomeClass.java
+//   run the program
+//      java WelcomeClass//
+
+//   define a class
+public class WelcomeClass{
+    
+//   add main method
+  public static void main(String[] args) {
+      
+//   print the statement 
+System.out.println("-----------");
+System.out.println("| WELCOME |");
+System.out.println("-----------");
+System.out.println("  ^  ^  ^  ^  ^  ^ ");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("<-M--P--L--2--1--9->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v");
+System.out.println("I broke my cheek bone in fifth grade. It left a dimple on my right cheek.");
+     
+     }
+}
